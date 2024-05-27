a65477bbaa3fae04d545a5580628b45a32909fd2
jespanap1 <jespanap@eafit.edu.co>
2024-05-27 11:34:51 -0500

    añadiendo app.js, plaza.js

--- app.js
+++ app.js
@@ -4,54 +4,58 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const expressSession = require('express-session');
 const cookieParser = require('cookie-parser');
-
 const plazasModel = require('./models/plazas');
 
 const app = express();
 
-// EJS como motor de plantilla
-app.set('view engine', 'ejs');
+// Middleware para parsear el cuerpo de las solicitudes como JSON
+app.use(express.json()); // <-- Esto es crucial
 
-app.set('views', [
-  path.join(__dirname, './views')
-]);
-
-// middlewares
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware para parsear el cuerpo de las solicitudes con URL encoded
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
+// Otros middlewares
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 app.use(morgan('tiny'));
 app.use(cookieParser());
 app.use(expressSession({ secret: 'este es mi secreto monito123' }));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Middleware para gestión de cookies
 app.use((req, res, next) => {
-  if(req.cookies.email){
-      const userModel = require('./models/user');
-
-      const user = userModel.findByEmail(req.cookies.email);
-
-      delete user.id;
-      delete user.password;
-
-      req.session.user = user;
+  if (req.cookies.email) {
+    const userModel = require('./models/user');
+    const user = userModel.findByEmail(req.cookies.email);
+    delete user.id;
+    delete user.password;
+    req.session.user = user;
   }
-
   next();
 });
 
-// requiriendo las rutas
+// Rutas
 const homeRoutes = require('./routes/homeRoutes');
 const loginRoutes = require('./routes/loginRoutes');
 const registerRoutes = require('./routes/registerRoutes');
 const parkRoutes = require('./routes/parkRoutes');
 
-// definiendo las rutas
-app.use('/', homeRoutes); // Ruta raíz
-app.use('/login', loginRoutes); // Ruta para el login
-app.use('/register', registerRoutes); // Ruta para el registro
+app.use('/', homeRoutes);
+app.use('/login', loginRoutes);
+app.use('/register', registerRoutes);
 app.use('/park', parkRoutes);
 
+// Ruta para actualizar la plaza 1
+app.post('/updatePlaza1', (req, res) => {
+  const { plazaOcupada } = req.body;
+  const ocupada = plazaOcupada === true;
+  plazasModel.actualizarPlaza1(ocupada);
+  res.sendStatus(200);
+});
+
+// Iniciar el servidor
 app.listen(3001, () => {
   console.log("Servidor corriendo en http://localhost:3001");
   plazasModel.modifyPlazaStates();
