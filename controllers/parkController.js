76264ff6a081cfcd4e44e0cdf4cd038ffe6cf274
jespanap1 <jespanap@eafit.edu.co>
2024-05-28 21:04:16 -0500

    esp32 finished

--- controllers/parkController.js
+++ controllers/parkController.js
@@ -7,7 +7,7 @@ const parkController = {
     const plazasData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/plazas.json'), 'utf8'));
 
     // Renderizar la vista y pasar los datos de las plazas
-    res.render('park', { plazas: plazasData });
+    res.render('park', { plazas: plazasData, distanceFromArduino: null });
   },
 
   receiveDistanceFromArduino: (req, res) => {
@@ -17,8 +17,25 @@ const parkController = {
     // Realizar cualquier procesamiento necesario con la distancia recibida
     console.log(`Distancia recibida del Arduino: ${distance}`);
     
-    // Pasar la distancia a la vista
-    res.render('park', { distanceFromArduino: distance }); // Agregamos un log para asegurarnos de que la distancia se está pasando correctamente
+    // Leer el archivo JSON de plazas
+    const plazasData = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/plazas.json'), 'utf8'));
+
+    // Actualizar el estado de plaza1 (plaza con id 1)
+    const plaza1 = plazasData.find(plaza => plaza.id === 1);
+
+    if (plaza1) {
+      const distanceCm = parseFloat(distance);
+      if (!isNaN(distanceCm)) {
+        plaza1.status = distanceCm < 20;
+        plaza1.lastModified = new Date().toISOString();
+      }
+    }
+
+    // Escribir los cambios de nuevo en el archivo JSON
+    fs.writeFileSync(path.join(__dirname, '../data/plazas.json'), JSON.stringify(plazasData, null, 2), 'utf8');
+    
+    // Renderizar la vista y pasar los datos actualizados de las plazas
+    res.render('park', { plazas: plazasData, distanceFromArduino: distance });
   }
 };
 
